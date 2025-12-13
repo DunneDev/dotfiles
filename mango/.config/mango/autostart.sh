54d5678dc17ad8d6bfc2703e0f3332c31ed9f3fb
Sebastien Dunne <dunn0203@algonquinlive.com>
2025-12-13 12:22:53 -0500

    added mango

---
+++ mango/.config/mango/autostart.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# waybar -c ~/.config/mango/config.jsonc -s ~/.config/mango/style.css >/dev/null 2>&1 &
+mako >/dev/null 2>&1 &
+
+swaybg -i ~/.config/mango/img.png >/dev/null 2>&1 &
+kanata -c ~/.config/kanata/kanata.kbd >/dev/null 2>&1 &
